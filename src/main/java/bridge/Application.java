dee38a3b07aa572df2929d7693057dcf451d012e
cksdud8028 <cksdud8028@naver.com>
2022-11-22 14:01:45 +0900

    feat(Application): define controlException and use in main

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -2,11 +2,20 @@ package bridge;
 
 public class Application {
     public static void main(String[] args) {
-        startGame();
+        try {
+            startGame();
+        } catch (IllegalArgumentException exception) {
+            controlException();
+        }
+
     }
 
     public static void startGame() {
         GameController gameController = new GameController();
+    }
+
+    private static void controlException() {
+        System.out.println("[ERROR]");
     }
 
 }
