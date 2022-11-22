57d06c4f51b2eb01fea313e8b2ed599a0a8497c4
cksdud8028 <cksdud8028@naver.com>
2022-11-22 14:18:45 +0900

    feat(Application): define initialBridge and use in constructor

--- src/main/java/bridge/GameController.java
+++ src/main/java/bridge/GameController.java
@@ -1,5 +1,22 @@
 package bridge;
 
+import java.util.List;
+
 public class GameController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final int bridgeSize;
+    private final List<String> bridge;
+
+    public GameController() {
+        bridgeSize = inputView.readBridgeSize();
+        bridge = initialBridge();
+    }
+
+    private List<String> initialBridge() {
+        BridgeNumberGenerator randomNumberGenerator = new BridgeRandomNumberGenerator();
+        BridgeMaker bridgeMaker = new BridgeMaker(randomNumberGenerator);
+        return bridgeMaker.makeBridge(bridgeSize);
+    }
 
 }
